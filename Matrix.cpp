91e423e881c20a1cfe775a0d1b4c62e1563ba76f
Mitrandir15 <bilmaksim13@gmail.com>
2018-11-05 00:00:42 +0200

    невеличкі зміни

--- Matrix.cpp
+++ Matrix.cpp
@@ -5,132 +5,132 @@ Matrix::Matrix() : matr(nullptr), n(0), m(0)
 {
 }
 
-Matrix::Matrix(int row, int column, int numb = 0) :n(row), m(column)
+Matrix::Matrix(int row, int column, int numb = 0) : n(row), m(column)
 {
-    matr = new int*[n];
-    for (int i = 0; i < n; i++)
-    {
-        *(matr + i) = new int[m];
-        for (int j = 0; j < m; j++)
-        {
-            *(*(matr + i) + j) = numb;
-        }
-    }
+	matr = new int*[n];
+	for (int i = 0; i < n; i++)
+	{
+		*(matr + i) = new int[m];
+		for (int j = 0; j < m; j++)
+		{
+			*(*(matr + i) + j) = numb;
+		}
+	}
 }
 
 Matrix::Matrix(const Matrix& other) : n(other.n), m(other.m)
 {
-    matr = new int*[n];
-    for (int i = 0; i < n; i++)
-    {
-        *(matr + i) = new int[m];
-        for (int j = 0; j < m; j++)
-        {
-            *(*(matr + i) + j) = *(*(other.matr + i) + j);
-        }
-    }
+	matr = new int*[n];
+	for (int i = 0; i < n; i++)
+	{
+		*(matr + i) = new int[m];
+		for (int j = 0; j < m; j++)
+		{
+			*(*(matr + i) + j) = *(*(other.matr + i) + j);
+		}
+	}
 }
 
 Matrix::Matrix(Matrix&& other)
 {
-    n = other.n;
-    m = other.m;
-    matr = new int*[n];
-    for (int i = 0; i < n; i++)
-    {
-        *(matr + i) = new int[m];
-        for (int j = 0; j < m; j++)
-        {
-            *(*(matr + i) + j) = *(*(other.matr + i) + j);
-        }
-    }
-    other.n = 0;
-    other.m = 0;
-    other.matr = nullptr;
+	n = other.n;
+	m = other.m;
+	matr = new int*[n];
+	for (int i = 0; i < n; i++)
+	{
+		*(matr + i) = new int[m];
+		for (int j = 0; j < m; j++)
+		{
+			*(*(matr + i) + j) = *(*(other.matr + i) + j);
+		}
+	}
+	other.n = 0;
+	other.m = 0;
+	other.matr = nullptr;
 }
 
 Matrix::~Matrix()
 {
-    for (int i = 0; i < n; i++)
-    {
-        delete[] *(matr + i);
-    }
-    delete [] matr;
+	for (int i = 0; i < n; i++)
+	{
+		delete[] * (matr + i);
+	}
+	delete[] matr;
 }
 
 void Matrix::setMatrix()
 {
-    cout << "Введіть розмір матриці: рядків  "; 
-	cin >> n; 
-	cout << " стовпчиків "; 
+	cout << "Введіть розмір матриці: рядків  ";
+	cin >> n;
+	cout << " стовпчиків ";
 	cin >> m;
-    matr = new int*[n];
-    for (int i = 0; i < n; i++)
-    {
-        *(matr + i) = new int[m];
-        for (int j = 0; j < m; j++)
-        {
-            cout << "Елемент " << i + 1 << " рядка, " << j + 1 << "стовпчика ";
-            cin >> *(*(matr + i) + j);
-        }
-    }
+	matr = new int*[n];
+	for (int i = 0; i < n; i++)
+	{
+		*(matr + i) = new int[m];
+		for (int j = 0; j < m; j++)
+		{
+			cout << "Елемент " << i + 1 << " рядка, " << j + 1 << "стовпчика ";
+			cin >> *(*(matr + i) + j);
+		}
+	}
 
 }
 
 void Matrix::displayMatrix()const
 {
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cout << setw(4) << *(*(matr + i) + j);
-        }
-        cout << endl;
-    }
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << setw(4) << *(*(matr + i) + j);
+		}
+		cout << endl;
+	}
 }
 
 int* Matrix::getRow(int ind)const
 {
-    int* row = new int[m];
-    for (int i = 0; i < m; i++)
-    {
-        *(row + i) = *(*(matr + ind - 1) + i);
-    }
-    return row;
+	int* row = new int[m];
+	for (int i = 0; i < m; i++)
+	{
+		*(row + i) = *(*(matr + ind - 1) + i);
+	}
+	return row;
 }
 
 int* Matrix::getColumn(int ind)const
 {
-    int* column = new int[n];
-    for (int i = 0; i < n; i++)
-    {
-        *(column + i) = *(*(matr + i) + ind - 1);
-    }
-    return column;
+	int* column = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		*(column + i) = *(*(matr + i) + ind - 1);
+	}
+	return column;
 }
 
 Matrix Matrix::operator+(const Matrix& other) const
 {
-    if ((n == other.n) && (m == other.m))
-    {
-        Matrix result(n, m);
-        result.matr = new int*[n];
-        for (int i = 0; i < n; i++)
-        {
-            *(result.matr + i) = new int[m];
-            for (int j = 0; j < m; j++)
-            {
-                *(*(result.matr + i) + j) = *(*(matr + i) + j) + *(*(other.matr + i) + j);
-            }
-        }
-        return result;
-    }
-    else
-    {
+	if ((n == other.n) && (m == other.m))
+	{
+		Matrix result(n, m);
+		result.matr = new int*[n];
+		for (int i = 0; i < n; i++)
+		{
+			*(result.matr + i) = new int[m];
+			for (int j = 0; j < m; j++)
+			{
+				*(*(result.matr + i) + j) = *(*(matr + i) + j) + *(*(other.matr + i) + j);
+			}
+		}
+		return result;
+	}
+	else
+	{
 		cout << "Додавання нерівнорозмірних матриць неможливе" << endl;
-        system("pause");
-        exit(1);
-    }
+		system("pause");
+		exit(1);
+	}
 }
 
 Matrix Matrix::operator-(const Matrix& other) const
@@ -159,134 +159,134 @@ Matrix Matrix::operator-(const Matrix& other) const
 
 Matrix Matrix::operator* (int numb) const
 {
-    Matrix result(n, m);
-    result.matr = new int*[n];
-    for (int i = 0; i < n; i++)
-    {
-        *(result.matr + i) = new int[m];
-        for (int j = 0; j < m; j++)
-        {
-            *(*(result.matr + i) + j) = (*(*(matr + i) + j)) * numb;
-        }
-    }
-    return result;
+	Matrix result(n, m);
+	result.matr = new int*[n];
+	for (int i = 0; i < n; i++)
+	{
+		*(result.matr + i) = new int[m];
+		for (int j = 0; j < m; j++)
+		{
+			*(*(result.matr + i) + j) = (*(*(matr + i) + j)) * numb;
+		}
+	}
+	return result;
 }
 
 Matrix Matrix::operator*(const Matrix& other)const
 {
-    if (m == other.n)
-    {
-        int counter = m, ttl = 0;
-        Matrix result(n, other.m);
-        int *row;
-        int *column;
-        result.matr = new int*[n];
-        for (int i = 0; i < n; i++)
-        {
-            *(result.matr + i) = new int[other.m];
-            for (int j = 0; j < other.m; j++)
-            {
-                row = getRow(i + 1);
-                column = other.getColumn(j + 1);
-                for (int k = 0; k < counter; k++)
-                {
-                    ttl += (row[k] * column[k]);
-                }
-                *(*(result.matr + i) + j) = ttl;
-                ttl = 0;
-            }
-        }
-        return result;
-    }
-    else
-    {
-        cout << m << "   " << other.n;
-        cout << "Множення нерівнорозмірних матриць неможливе" << endl;
-        system("pause");
-        exit(1);
-    }
+	if (m == other.n)
+	{
+		int counter = m, temp = 0;
+		Matrix result(n, other.m);
+		int *row;
+		int *column;
+		result.matr = new int*[n];
+		for (int i = 0; i < n; i++)
+		{
+			*(result.matr + i) = new int[other.m];
+			for (int j = 0; j < other.m; j++)
+			{
+				row = getRow(i + 1);
+				column = other.getColumn(j + 1);
+				for (int k = 0; k < counter; k++)
+				{
+					temp += (row[k] * column[k]);
+				}
+				*(*(result.matr + i) + j) = temp;
+				temp = 0;
+			}
+		}
+		return result;
+	}
+	else
+	{
+		cout << "Множення нерівнорозмірних матриць неможливе" << endl;
+		system("pause");
+		exit(1);
+	}
 
 }
 
 Matrix& Matrix::operator=(const Matrix& other)
 {
-    if (this != &other)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            delete[] * (matr + i);
-        }
-        delete matr;
-        n = other.n;
-        m = other.m;
-        matr = new int*[n];
-        for (int i = 0; i < n; i++)
-        {
-            *(matr + i) = new int[m];
-            for (int j = 0; j < m; j++)
-            {
-                *(*(matr + i) + j) = *(*(other.matr + i) + j);
-            }
-        }
-    }
+	if (this != &other)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			delete[] * (matr + i);
+		}
+		delete matr;
+		n = other.n;
+		m = other.m;
+		matr = new int*[n];
+		for (int i = 0; i < n; i++)
+		{
+			*(matr + i) = new int[m];
+			for (int j = 0; j < m; j++)
+			{
+				*(*(matr + i) + j) = *(*(other.matr + i) + j);
+			}
+		}
+	}
 
-    return *this;
+	return *this;
 }
 
 Matrix& Matrix::operator=(Matrix&& other)
 {
-    if (this != &other)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            delete[] * (matr + i);
-        }
-        delete matr;
-        n = other.n;
-        m = other.m;
-        matr = other.matr;
-        other.n = 0;
-        other.m = 0;
-        other.matr = nullptr;
-    }
+	if (this != &other)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			delete[] * (matr + i);
+		}
+		delete matr;
+		n = other.n;
+		m = other.m;
+		matr = other.matr;
+		other.n = 0;
+		other.m = 0;
+		other.matr = nullptr;
+	}
 
-    return *this;
+	return *this;
 }
 
 bool Matrix::operator==(const Matrix& other)const
 {
-    if (n != other.n || m != other.m)
-        return false;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (*(*(matr + i) + j) != *(*(other.matr + i) + j))
-                return false;
-        }
-    }
-    return true;
+	if (n != other.n || m != other.m)
+		return false;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (*(*(matr + i) + j) != *(*(other.matr + i) + j))
+				return false;
+		}
+	}
+	return true;
 }
 int* Matrix::operator[](int i)const
 {
-    return getRow(i);
-}
+	return matr[i];
+};
+
 
 Matrix& Matrix::operator~()
 {
-    Matrix result(m, n);
-    int *column;
-    result.matr = new int*[m];
-    for (int i = 0; i < m; i++)
-    {
-        column = this->getColumn(i + 1);
-        *(result.matr + i) = new int[n];
-        for (int j = 0; j < n; j++)
-        {
-            *(*(result.matr + i) + j) = column[j];
-        }
-    }
-    return result;
+	Matrix result(m, n);
+	int *column;
+	result.matr = new int*[m];
+	for (int i = 0; i < m; i++)
+	{
+		column = this->getColumn(i + 1);
+		*(result.matr + i) = new int[n];
+		for (int j = 0; j < n; j++)
+		{
+			*(*(result.matr + i) + j) = column[j];
+		}
+	}
+	return result;
 }
 
 ostream& operator << (ostream& output, const Matrix& matrix)
--- main.cpp
+++ main.cpp
@@ -4,5 +4,5 @@ using namespace std;
 
 void main()
 {
-	//тут має бути код
+	// тут буде код
 }
